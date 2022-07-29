029d23bbecf10d8d2131e299758a9b562b31639c
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-07-29 20:26:13 +0500

    created main models

---
+++ src/CodeStorm.Domain/Models/CheckerInput.cs
@@ -0,0 +1,43 @@
+﻿namespace CodeStorm.Domain.Models
+{
+    /// <summary>
+    /// Foydalanuvchi tomonidan kiritilishi kerak bo'lgan ma'lumotlar
+    /// </summary>
+    public class CheckerInput
+    {
+        /// <summary>
+        /// Tekshirilishi kerak bo'lgan kod yozilgan til
+        /// </summary>
+        public string Language { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Tekshirilishi kerak bo'lgan kod manzili
+        /// </summary>
+        public string SourceCodePath { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Tekshirilishi kerak bo'lgan kodni testlari manzili
+        /// ushbu kodni tekshirish uchun yozib qo'yilgan 
+        /// input va output lar yig'ilgan joy
+        /// </summary>
+        public string ProblemSetPath { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Kodni qayerda kompilatsiya qilish kerakligi, 
+        /// va qayerda uni boshqarish kerakligi manzili
+        /// </summary>
+        public string MissionPath { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Har bir test uchun xotiradan ishlatilishi mumkin bo'lgan maksimum qiymat
+        /// o'lchov birligi MB larda
+        /// </summary>
+        public ushort MemoryLimit { get; set; }
+
+        /// <summary>
+        /// Har bir test uchun ishlatilishi mumkin bo'lgan maksimum vaqt
+        /// o'lchov birligi millisecond larda
+        /// </summary>
+        public ushort TimeLimit { get; set; }
+    }
+}
---
+++ src/CodeStorm.Domain/Models/CheckerResult.cs
@@ -0,0 +1,46 @@
+﻿using CodeStorm.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeStorm.Domain.Models
+{
+    /// <summary>
+    /// Dastur qaytaradigan asosiy natija uchun
+    /// </summary>
+    public class CheckerResult
+    {
+        /// <summary>
+        /// Source code ni testlardan o'tkazilgandagi natijani qaytaradi
+        /// </summary>
+        public ResultType Result { get; set; }
+
+        /// <summary>
+        /// Yuborilgan kod nechta testdan o'tganligini bildiradi
+        /// </summary>
+        public int TestNumber { get; set; }
+
+        /// <summary>
+        /// Dasturni kompilatsiya qilishda va testlashda chiqqan xatolar uchun
+        /// </summary>
+        public string ErrorMessage { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Yuborilgan kodni har bir test qilinganda 
+        /// input qiymat kirishi va output qaytarishi orasidagi vaqtlar o'lchanadi
+        /// o'lchov birligi - millisecond larda
+        /// </summary>
+        public Dictionary<int, long> ProcessingTimes { get; set; }
+            = new Dictionary<int, long>();
+
+        /// <summary>
+        /// Yuborilgan kodni har bir test qilinganda
+        /// foydalanuvchi kodi har bir test uchun ishlatadigan xotira hajmi o'lchanadi
+        /// o'lchov birligi - Kb larda
+        /// </summary>
+        public Dictionary<int, long> MemoryUsages { get; set; }
+            = new Dictionary<int, long>();
+    }
+}
